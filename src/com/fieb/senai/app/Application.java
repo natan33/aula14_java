d6852f026b5e597e0484857d7e776e69d8da99a0
natan33 <srassis2014@gmail.com>
2018-12-06 21:22:19 -0300

    quarto commit

--- src/com/fieb/senai/app/Application.java
+++ src/com/fieb/senai/app/Application.java
@@ -3,6 +3,9 @@ package com.fieb.senai.app;
 
 import com.fieb.senai.entidades.Endereco;
 import com.fieb.senai.entidades.Pessoa;
+import com.fieb.senai.entidades.Telefone;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Application {
@@ -17,10 +20,25 @@ public class Application {
       
       Pessoa p2 = new Pessoa("Jhon", 48516, 35);
       Endereco end2 = new Endereco("Rua 16 de janeiro","21CAE","Coutos");
+      Telefone tel2 =  new Telefone("789456");
       
       //Declarando a associação.
       p2.setEndereco(end2);
+      p2.setTelefone(tel2);
       
+      Pessoa p3 =  new Pessoa("joãozinho", 148516, 15);
+      Telefone tel1 =  new Telefone("123456");
+      Telefone tel3 =  new Telefone("12345615");
+      
+      //Declaração de associação. 
+      p3.setTelefone(tel1);
+      
+      List<Telefone> listatelefone =  new ArrayList<>();
+      
+        for (Telefone telefone : listatelefone) {
+            System.out.println("Telefone: " + telefone.getNumero()); 
+            
+        }
     }
     
 }
